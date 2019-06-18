1f99bcef0b88b0511669874955c01d899114f164
Patrick Horton <available@upon.request>
2019-06-18 03:44:46 -0500

    SGTL5000 (Teensy Audio Shield) control

--- audio/examples/02-StereoPassThru/02-StereoPassThru.cpp
+++ audio/examples/02-StereoPassThru/02-StereoPassThru.cpp
@@ -1,21 +1,47 @@
 
 #include <audio\Audio.h>
 
-#define USE_CS42448  1
+// You may only define zero or one of the following
+// the default is wm8731
+
+#define USE_CS42448   0
+#define USE_STGL5000  1
 
 #if USE_CS42448
 
+    // Octo is always the master
+    
     AudioInputTDM input;
     AudioOutputTDM output;
     AudioControlCS42448 control;
 
-#else
+#elif USE_STGL5000  // only in slave mode
 
-    #define I2S_MASTER   0
-        // is the BCM_PCM the master or slave device
-        // Paul usually thinks the teensy is the master device,
-        // and adds suffixes to the class names when it's not.
-        // The rpi behaves better as the i2s clave than as the master.
+    // #if I2S_MASTER
+    // the rpi cannot be a master to an sgtl5000.
+    // the sgtl5000 requires 3 clocks and the rpi can only generate 2
+    //
+    // AudioInputI2S input;
+    // AudioOutputI2S output;
+    // AudioControlSGTL5000 control;
+    // #else
+    
+    AudioInputI2Sslave input;
+    AudioOutputI2Sslave output;
+    AudioControlSGTL5000master control;
+
+    // #endif
+    
+#else   // wm8731 in master or slave mode
+
+    #define I2S_MASTER    0
+
+    // Is the BCM_PCM the master or slave device?
+    // Of the devices so far, only the wm8731 can be a master.
+    // Paul usually thinks the teensy is the master device,
+    // and adds suffixes to the class names when it's not, but
+    // the rpi generally behaves better as the i2s slave than
+    // as the master.
     
     #if I2S_MASTER
         AudioInputI2S input;
@@ -26,13 +52,11 @@
         AudioOutputI2Sslave output;
         AudioControlWM8731master control;
     #endif
-
 #endif
 
 
 AudioConnection  c0(input, 0, output, 0);
 AudioConnection  c1(input, 0, output, 1);
-
 
 
 
@@ -50,7 +74,6 @@ void setup()
     #endif
     
     control.volume(1.0);
-
     
     printf("02-StereoPassThru::setup() finished\n");
 }
