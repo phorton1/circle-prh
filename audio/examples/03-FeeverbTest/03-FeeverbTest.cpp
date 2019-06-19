94e909ce4835cd7357a95e7f5059a2f014339329
Patrick Horton <available@upon.request>
2019-06-19 17:03:42 -0500

    return to baseline wm8731 reverb tests

--- audio/examples/03-FeeverbTest/03-FeeverbTest.cpp
+++ audio/examples/03-FeeverbTest/03-FeeverbTest.cpp
@@ -1,29 +1,22 @@
 
 #include <audio\Audio.h>
 
-// There is a funny twang every so often in FreeVerb,
-// maybe having to do with integer overflows.
+// you may define 0 or 1 of USE_REVERB and USE_FREEVERB
+// USE_MIXER puts the output through a mixer first
 
-#define USE_MIXER       0
+#define USE_REVERB      1
 #define USE_FREEVERB    0
+#define USE_MIXER       1
+    
 
-
-#define USE_CS42448  1
+#define USE_CS42448  0
 
 #if USE_CS42448
-
     AudioInputTDM input;
     AudioOutputTDM output;
     AudioControlCS42448 control;
-    AudioControlCS42448 *pControl;
-
 #else
-
-    #define I2S_MASTER   1
-
-    // There seems to be some noise on the right channel
-    // with the rpi in master mode with the wm8731 
-    
+    #define I2S_MASTER   0
     #if I2S_MASTER
         AudioInputI2S input;
         AudioOutputI2S output;
@@ -38,50 +31,35 @@
 
 
 #if USE_FREEVERB
-    AudioEffectFreeverbStereo   reverb;
+    AudioEffectFreeverbStereo reverb;
+    #define SECOND_CHANNEL  1
+    AudioConnection c0(input, 0, reverb, 0);
+    AudioConnection c1(input, 1, reverb, 1);
+#elif USE_REVERB
+    AudioEffectReverb reverb;
+    AudioConnection c0(input, 0, reverb, 0);
+    #define SECOND_CHANNEL  0
 #else
-    AudioEffectReverb   reverb1;
-    AudioEffectReverb   reverb2;
+    #define reverb input
+    #define SECOND_CHANNEL  1
 #endif
+
 
 #if USE_MIXER
     AudioMixer4 mixer1;
     AudioMixer4 mixer2;
-#endif
-
-
-#if USE_FREEVERB
-    AudioConnection  c1(input, 0, reverb, 0);
-    AudioConnection  c2(input, 1, reverb, 1);
+    AudioConnection c2(input,  0, mixer1, 0);
+    AudioConnection c3(input,  1, mixer2, 0);
+    AudioConnection c4(reverb, 0, mixer1, 1);
+    AudioConnection c5(reverb, SECOND_CHANNEL, mixer2, 1);
+    AudioConnection c6(mixer1, 0, output, 0);
+    AudioConnection c7(mixer2, 0, output, 1);
 #else
-    AudioConnection  c1(input, 0, reverb1, 0);
-    AudioConnection  c2(input, 1, reverb2, 0);
+    AudioConnection c2(reverb, 0, output, 0);
+    AudioConnection c3(reverb, SECOND_CHANNEL, output, 1);
 #endif
 
 
-#if USE_MIXER
-    AudioConnection  c3(input,   0, mixer1, 0);
-    AudioConnection  c4(input,   1, mixer2, 0);
-
-    #if USE_FREEVERB
-        AudioConnection  c5(reverb,  0, mixer1, 1);
-        AudioConnection  c6(reverb,  1, mixer2, 1);
-    #else
-        AudioConnection  c5(reverb1, 0, mixer1, 1);
-        AudioConnection  c6(reverb2, 0, mixer2, 1);
-    #endif
-
-    AudioConnection  c7(mixer1,  0, output, 0);
-    AudioConnection  c8(mixer2,  0, output, 1);
-#else
-    #if USE_FREEVERB
-        AudioConnection  c7(reverb,  0, output, 0);
-        AudioConnection  c8(reverb,  1, output, 1);
-    #else
-        AudioConnection  c7(reverb1, 0, output, 0);
-        AudioConnection  c8(reverb2, 0, output, 1);
-    #endif
-#endif
 
 
 void setup()
@@ -91,16 +69,10 @@ void setup()
     #if USE_FREEVERB
         reverb.roomsize(0.6);
         reverb.damping(0.5);
-    #else
-        reverb1.reverbTime(0.8);
-        reverb2.reverbTime(0.8);
+    #elif USE_REVERB
+        reverb.reverbTime(0.8);
     #endif
 
-    #if USE_CS42448
-        pControl = &control;
-        control.reset();    // the reset must be done before enable
-    #endif
-    
     control.enable();
         
     AudioMemory(100);
@@ -108,8 +80,9 @@ void setup()
     #if !USE_CS42448
         control.inputSelect(AUDIO_INPUT_LINEIN);
         control.inputLevel(1.0);
-        control.volume(1.0);
     #endif
+
+    control.volume(1.0);
         
     #if USE_MIXER
         mixer1.gain(0, 0.6);
