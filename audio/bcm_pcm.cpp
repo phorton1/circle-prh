faf67ba72bc15ae0d8b0d0525ef12c6e3c30bace
phorton1 <available@upon.request>
2020-09-14 04:34:08 -0500

    Looper/Teensy Circuit board - Change ports for Looper circuit board

--- audio/bcm_pcm.cpp
+++ audio/bcm_pcm.cpp
@@ -33,6 +33,7 @@
 #define PIN_RX_ACTIVE  12		// a LED to show recv activity
 #define PIN_TX_ACTIVE  16		// a LED to show xmit activity
 
+#define CALLS_PER_LED_FLASH 64			// about 10 times a second at 44.1khz with 128 sized buffers
 
 // This CLOCK_FREQ is used if we are the i2s master.
 
@@ -87,22 +88,22 @@ BCM_PCM::BCM_PCM() :
 		// GPIOClockSourcePLLC       = 5,		// 1000 MHz (changes with overclock settings)
 		// GPIOClockSourcePLLD       = 6,		// 500 MHz hence the #define CLOCK_FREQ 500000000
 		// GPIOClockSourceHDMI       = 7		// 216 MHz
-	#if INCLUDE_ACTIVITY_LEDS		
+	#if INCLUDE_ACTIVITY_LEDS
 		m_RX_ACTIVE(PIN_RX_ACTIVE, GPIOModeOutput),
 		m_TX_ACTIVE(PIN_TX_ACTIVE, GPIOModeOutput),
 	#endif
 	#if USE_LITE_DMA_CHANNEL
-		m_nDMAInChannel(CMachineInfo::Get()->AllocateDMAChannel(DMA_CHANNEL_LITE)),		
-		m_nDMAOutChannel(CMachineInfo::Get()->AllocateDMAChannel(DMA_CHANNEL_LITE))		
+		m_nDMAInChannel(CMachineInfo::Get()->AllocateDMAChannel(DMA_CHANNEL_LITE)),
+		m_nDMAOutChannel(CMachineInfo::Get()->AllocateDMAChannel(DMA_CHANNEL_LITE))
 	#else
-		m_nDMAInChannel(CMachineInfo::Get()->AllocateDMAChannel(DMA_CHANNEL_NORMAL)),		
-		m_nDMAOutChannel(CMachineInfo::Get()->AllocateDMAChannel(DMA_CHANNEL_NORMAL))		
+		m_nDMAInChannel(CMachineInfo::Get()->AllocateDMAChannel(DMA_CHANNEL_NORMAL)),
+		m_nDMAOutChannel(CMachineInfo::Get()->AllocateDMAChannel(DMA_CHANNEL_NORMAL))
 	#endif
 {
 	#ifdef DYNAMIC_INITIALIZATION
 		// If the object is statically initialized, we depend on BSS
-		// initialization to set everything to zero.  
-	
+		// initialization to set everything to zero.
+
 		m_as_slave = false;
 		m_SAMPLE_RATE = 0;
 		m_SAMPLE_SIZE = 0;
@@ -117,18 +118,18 @@ BCM_PCM::BCM_PCM() :
 		// it *may* happen that the ctor of this is called
 		// AFTER the ctor's of the teensy objects, and the
 		// stashed pointers get wiped out ...
-		// 
+		//
 		// m_inISR = 0;
 		// m_outISR = 0;
-		
+
 		m_state = bcmSoundIdle;
 		m_initialized = false;
 		m_bInIRQConnected = false;
 		m_bOutIRQConnected = false;
-		
+
 		m_inToggle = 0;
 		m_outToggle = 0;
-		
+
 		for (u8 i=0; i<2; i++)
 		{
 			m_inBuffer[i] = 0;
@@ -136,28 +137,28 @@ BCM_PCM::BCM_PCM() :
 			m_allocInBuffer[i] = 0;
 			m_allocOutBuffer[i] = 0;
 		}
-		
+
 		wrong_irq_count	= 0;
 		in_block_count 	= 0;
 		out_block_count = 0;
 		underflow_count = 0;
-		overflow_count 	= 0;	
+		overflow_count 	= 0;
 	#endif
-	
-	#if INCLUDE_ACTIVITY_LEDS		
+
+	#if INCLUDE_ACTIVITY_LEDS
 		m_RX_ACTIVE.Write(1);
 		m_TX_ACTIVE.Write(1);
-	#endif	
+	#endif
 }
 
-	
-	
+
+
 BCM_PCM::~BCM_PCM()
 {
 	if (m_initialized)
 		terminate();
 	m_pInterruptSystem = 0;
-	
+
 	for (int i=0; i<2; i++)
 	{
 		if (m_allocInBuffer[i])
@@ -181,23 +182,23 @@ void BCM_PCM::initBuffers()
 {
 	m_inToggle = 0;
 	m_outToggle = 0;
-	
+
 	for (int i=0; i<2; i++)
 	{
 		if (!m_inBuffer[i])
 			m_inBuffer[i] = allocateRawAudioBlock(&m_allocInBuffer[i]);
 		if (!m_outBuffer[i])
 			m_outBuffer[i] = allocateRawAudioBlock(&m_allocOutBuffer[i]);
-			
+
 		#if TEST_ROUTE_IN_TO_OUT
 			if (!test_buffer[i])
 				test_buffer[i] = allocateRawAudioBlock(&test_alloc[i]);
 		#endif
-			
+
 		memset(m_inBuffer[i],0,RAW_AUDIO_BLOCK_BYTES);
 		memset(m_outBuffer[i],0,RAW_AUDIO_BLOCK_BYTES);
 	}
-	
+
 	in_irq_count	= 0;
 	out_irq_count   = 0;
 	in_block_count  = 0;
@@ -233,7 +234,7 @@ uint32_t *BCM_PCM::allocateRawAudioBlock(u8 **allocBlock)
 // initialization
 //-----------------------------
 
-	
+
 void BCM_PCM::init()
 {
 	assert(m_inISR || m_outISR);
@@ -242,17 +243,17 @@ void BCM_PCM::init()
 	if (m_state >= bcmSoundRunning)
 	{
 		PCM_LOG("BCM_PCM::init(%d) returning because state=%d",as_slave,m_state);
-		return;		
+		return;
 	}
-	
+
 	m_pInterruptSystem = CInterruptSystem::Get();
 	assert(m_pInterruptSystem);
 
-	#if INCLUDE_ACTIVITY_LEDS		
+	#if INCLUDE_ACTIVITY_LEDS
 		m_RX_ACTIVE.Write(0);
 		m_TX_ACTIVE.Write(0);
 	#endif
-		
+
 	LOG("init(%d,%d,%d,%d,%d,%d,%d,%d,%d.%s) ...",
 		m_as_slave,
 		m_SAMPLE_RATE,
@@ -267,7 +268,7 @@ void BCM_PCM::init()
 		((u32)m_inISR) ? "INPUT" :
 		((u32)m_outISR) ? "OUTPUT" :
 		"ERROR - no isr specified in init!");
-	
+
 	LOG("inDMAChannel=%d outDMAChannel=%d",
 		m_nDMAInChannel,
 		m_nDMAOutChannel);
@@ -275,9 +276,9 @@ void BCM_PCM::init()
 	PCM_LOG("m_inControlBlock[1]=0x%08x",&m_inControlBlock[1]);
 	PCM_LOG("m_outControlBlock[0]=0x%08x",&m_outControlBlock[0]);
 	PCM_LOG("m_outControlBlock[1]=0x%08x",&m_outControlBlock[1]);
-	
+
 	// initialize the memory buffers
-	
+
 	initBuffers();
 	PCM_LOG("m_inBuffer[0]=0x%08x",m_inBuffer[0]);
 	PCM_LOG("m_inBuffer[1]=0x%08x",m_inBuffer[1]);
@@ -305,7 +306,7 @@ void BCM_PCM::init()
 	}
 
 	// start the BCLK
-	
+
 	if (!m_as_slave)
 	{
 		LOG("starting pcm BCLK",0);
@@ -314,7 +315,7 @@ void BCM_PCM::init()
 		unsigned nTemp = CLOCK_FREQ / (m_NUM_CHANNELS * m_CHANNEL_WIDTH) % m_SAMPLE_RATE;
 		unsigned nDivF = (nTemp * 4096 + m_SAMPLE_RATE/2) / m_SAMPLE_RATE;
 		assert(nDivF <= 4096);
-		
+
 		if (nDivF > 4095)
 		{
 			nDivI++;
@@ -323,14 +324,14 @@ void BCM_PCM::init()
 
 		m_BitClock.Start(nDivI, nDivF, nDivF > 0 ? 1 : 0);
 	}
-	
+
 	// initialize the I2S peripheral
 	// pre-enable DMA
-	
+
 	initFrame();
 	PeripheralEntry();
 	PCM_LOG("pre-enable DMA ...",0);
-	
+
 	if (m_inISR)
 	{
 		write32(ARM_DMA_ENABLE, read32(ARM_DMA_ENABLE) | (1 << m_nDMAInChannel));
@@ -345,12 +346,12 @@ void BCM_PCM::init()
 		write32(ARM_DMACHAN_CS(m_nDMAOutChannel), CS_RESET);
 		while (read32(ARM_DMACHAN_CS(m_nDMAOutChannel)) & CS_RESET) {}
 	}
-	
+
 	PeripheralExit();
 
 	PCM_LOG("init() finished",0);
 	m_initialized = true;
-	
+
 }	// init()
 
 
@@ -358,7 +359,7 @@ void BCM_PCM::init()
 void BCM_PCM::terminate()
 {
 	PCM_LOG("terminate(%d) ...",m_state);
-	
+
 	if (m_state == bcmSoundRunning)
 	{
 		PCM_LOG("terminate(%d) calling stop()",m_state);
@@ -368,7 +369,7 @@ void BCM_PCM::terminate()
 			   m_state == bcmSoundTerminating );
 		PCM_LOG("terminate() state=%d after calling stop()",m_state);
 	}
-	
+
 	stopPCM();
 
 	if (m_inISR)
@@ -378,7 +379,7 @@ void BCM_PCM::terminate()
 		while (read32(ARM_DMACHAN_CS(m_nDMAInChannel)) & CS_RESET) {}
 		write32(ARM_DMA_ENABLE, read32(ARM_DMA_ENABLE) & ~(1 << m_nDMAInChannel));
 		PeripheralExit();
-		
+
 		if (m_bInIRQConnected)
 		{
 			m_pInterruptSystem->DisconnectIRQ(ARM_IRQ_DMA0+m_nDMAInChannel);
@@ -387,7 +388,7 @@ void BCM_PCM::terminate()
 		CMachineInfo::Get()->FreeDMAChannel(m_nDMAInChannel);
 		m_nDMAInChannel = 0;
 	}
-	
+
 	if (m_outISR)
 	{
 		PeripheralEntry();
@@ -413,9 +414,9 @@ void BCM_PCM::terminate()
 	m_inISR = 0;
 	m_outISR = 0;
 	m_initialized = false;
-	
+
 	PCM_LOG("terminate(%d) finished",m_state);
-	
+
 }	// terminate()
 
 
@@ -428,7 +429,7 @@ void BCM_PCM::initDMA(
 {
 	// Set the generic Transfer Information bits
 
-	u32 tInfo = 
+	u32 tInfo =
 		(DEFAULT_BURST_LENGTH << TI_BURST_LENGTH_SHIFT)
 		| TI_WAIT_RESP
 		| TI_INTEN;
@@ -440,16 +441,16 @@ void BCM_PCM::initDMA(
 		tInfo |= 0
 			| (DREQSourcePCMTX << TI_PERMAP_SHIFT)
 			| TI_SRC_WIDTH
-			| TI_SRC_INC 
+			| TI_SRC_INC
 			| TI_DEST_DREQ;
-			
-		#if TEST_SHORT_CIRCUIT_IN_TO_OUT			
+
+		#if TEST_SHORT_CIRCUIT_IN_TO_OUT
 			if (m_inISR)
 				pb[id].nSourceAddress = BUS_ADDRESS((uintptr) m_inBuffer[id]);
 			else
 		#endif
-		
-		pb[id].nSourceAddress = BUS_ADDRESS((uintptr) m_outBuffer[id]);	
+
+		pb[id].nSourceAddress = BUS_ADDRESS((uintptr) m_outBuffer[id]);
 		pb[id].nDestinationAddress = (ARM_PCM_FIFO_A & 0xFFFFFF) + GPU_IO_BASE;
 	}
 	else	// Set for input (bo is zero)
@@ -460,29 +461,29 @@ void BCM_PCM::initDMA(
 			| TI_DEST_INC
 			| TI_SRC_DREQ;	// DMA gated by PCM RX signal
 		pb[id].nSourceAddress = (ARM_PCM_FIFO_A & 0xFFFFFF) + GPU_IO_BASE;   // PRH
-		pb[id].nDestinationAddress = BUS_ADDRESS((uintptr) m_inBuffer[id]);	
+		pb[id].nDestinationAddress = BUS_ADDRESS((uintptr) m_inBuffer[id]);
 	}
 
 	// finish up
-	
+
 	pb[id].nTransferInformation   	= tInfo;
 	pb[id].nTransferLength 			= RAW_AUDIO_BLOCK_BYTES;
 	pb[id].n2DModeStride       		= 0;
 	pb[id].nReserved[0]	       		= 0;
 	pb[id].nReserved[1]	       		= 0;
 	pb[id].nNextControlBlockAddress = BUS_ADDRESS((uintptr) &pb[other_id]);
-	
+
 	CleanAndInvalidateDataCacheRange((uintptr) &pb[id], sizeof (TDMAControlBlock));
 
 }	// initDMA
-	
+
 
 
 void BCM_PCM::initFrame()
 {
 	PeripheralEntry();
 	PCM_LOG("initFrame() ...",0);
-	
+
 	// Write zero which turns everything off
 	// and clear the PCM peipheral FIFOs
 
@@ -490,17 +491,17 @@ void BCM_PCM::initFrame()
 	CTimer::Get()->usDelay(10);
 	write32(ARM_PCM_CS_A, read32(ARM_PCM_CS_A) | CS_A_TXCLR | CS_A_RXCLR);
 	CTimer::Get()->usDelay(10);
-	
+
 	// Setup the TXC_A and RXC_A registers (PCM frame definition)
-	
+
 	u8 set_extended_bit = 0;
 	u32 use_width = m_SAMPLE_SIZE - 8;					// 8 bits are implied
-	if (use_width >= 16)								// if larger than 16 
+	if (use_width >= 16)								// if larger than 16
 	{
 		set_extended_bit = 1;							// set the 1 Channel Width Extension bits
 		use_width -= 16;								// and subtract 16
 	}
-	
+
 	// Different devices may uses different CHANNEL_OFFSET values.
 	// At this time, I know of
 	//
@@ -519,7 +520,7 @@ void BCM_PCM::initFrame()
 		| (m_CHANNEL1_OFFSET << TXC_A_CH1POS__SHIFT)	// the BCLK on which the channel data starts
 		| (set_extended_bit ? TXC_A_CH1WEX : 0) 		// Set the "extended width" bit (if width is 16+)
 		| (use_width << TXC_A_CH1WID__SHIFT)			// Set the low 4 channel width bits
-		| TXC_A_CH2EN									// Same for channel 2 
+		| TXC_A_CH2EN									// Same for channel 2
 		| ((m_CHANNEL_WIDTH + m_CHANNEL2_OFFSET) << TXC_A_CH2POS__SHIFT) // i2s standard skips 1 bit after LR change
 		| (set_extended_bit ? TXC_A_CH2WEX : 0)
 		| (use_width << TXC_A_CH2WID__SHIFT);
@@ -528,7 +529,7 @@ void BCM_PCM::initFrame()
 		write32(ARM_PCM_TXC_A,reg_val);
 	if (m_inISR)
 		write32(ARM_PCM_RXC_A,reg_val);
-	
+
 	// set the PCM_MODE_A register
 	// Invert BCLK and/or FCLK
 	// See devices for required settings
@@ -538,10 +539,10 @@ void BCM_PCM::initFrame()
 		pcm_mode |= MODE_A_CLKI;	// Invert the BCLK signal sense
 	if (m_INVERT_FCLK)
 		pcm_mode |= MODE_A_FSI;		// Frame Sync Invert
-	
+
 	// set slave or master mode
-	
-	if (m_as_slave)	
+
+	if (m_as_slave)
 	{
 		pcm_mode |= MODE_A_CLKM;	// BCLK is an input
 		pcm_mode |= MODE_A_FSM;		// FCLK is an input
@@ -565,7 +566,7 @@ void BCM_PCM::initFrame()
 	CTimer::Get()->usDelay(50);
 	write32(ARM_PCM_CS_A, read32(ARM_PCM_CS_A) | CS_A_EN);
 	CTimer::Get()->usDelay(10);
-	
+
 	PeripheralExit();
 	PCM_LOG("initFrame() finished",0);
 }
@@ -574,7 +575,7 @@ void BCM_PCM::initFrame()
 void BCM_PCM::stopPCM(void)
 {
 	PCM_LOG("stopPCM() ...",0);
-	
+
 	PeripheralEntry();
 	write32(ARM_PCM_CS_A, 0);
 	CTimer::Get()->usDelay(50);
@@ -593,7 +594,7 @@ void BCM_PCM::stop()
 	if (m_state != bcmSoundRunning)
 	{
 		PCM_LOG("stop() returning because state=%d\n",m_state);
-		return;		
+		return;
 	}
 
 	m_SpinLock.Acquire();
@@ -616,21 +617,21 @@ void BCM_PCM::start()
 	if (m_state != bcmSoundIdle)
 	{
 		PCM_LOG("BCM_PCM::start() returning because state=%d",m_state);
-		return;		
+		return;
 	}
-	
+
 	PCM_LOG("start() ...",0);
-	
-	#if INCLUDE_ACTIVITY_LEDS		
+
+	#if INCLUDE_ACTIVITY_LEDS
 		if (m_inISR)
 		 	m_RX_ACTIVE.Write(1);
 		if (m_outISR)
 			m_TX_ACTIVE.Write(1);
 	#endif
-	
+
 	// re-initialize the memory buffers
-	
-	initBuffers();		
+
+	initBuffers();
 
 	// connect IRQs
 	//
@@ -638,7 +639,7 @@ void BCM_PCM::start()
 	// the output isr routine gets called (last one in order of code below?).
 	// When using normal DMA channels, the interrupts are separate, but the
 	// dma is more finicky.
-	
+
 	if (m_inISR && !m_bInIRQConnected)
 	{
 		PCM_LOG("connecting input IRQ",0);
@@ -668,40 +669,40 @@ void BCM_PCM::start()
 	}
 
 	// start the clock HERE if needed
-	
+
 	#ifdef __circle__
 		if (m_startClock)
 			(*m_startClock)();
 	#endif
-	
+
 	// enable I2S DMA operation
-	
+
 	PeripheralEntry();
 	write32(ARM_PCM_CS_A, read32(ARM_PCM_CS_A) | CS_A_DMAEN);
 	PeripheralExit();
 
 	// start DMA
-	
+
 	PeripheralEntry();
 	if (m_inISR)
 	{
 		// PCM_LOG("BUS_ADDRESS(0x%08x)=0x%08x",(u32)&m_inControlBlock[0],BUS_ADDRESS((uintptr)&m_inControlBlock[0]));
-		
+
 		write32(ARM_DMACHAN_CONBLK_AD(m_nDMAInChannel),
 			BUS_ADDRESS((uintptr) &m_inControlBlock[0]));
-	
+
 		write32(ARM_DMACHAN_CS(m_nDMAInChannel),
 			CS_WAIT_FOR_OUTSTANDING_WRITES
 			| (DEFAULT_PANIC_PRIORITY << CS_PANIC_PRIORITY_SHIFT)
 			| (DEFAULT_PRIORITY << CS_PRIORITY_SHIFT)
 			| CS_ACTIVE);
 	}
-	
+
 	if (m_outISR)
 	{
 		write32(ARM_DMACHAN_CONBLK_AD(m_nDMAOutChannel),
 			BUS_ADDRESS((uintptr) &m_outControlBlock[0]));
-	
+
 		write32(ARM_DMACHAN_CS(m_nDMAOutChannel),
 			CS_WAIT_FOR_OUTSTANDING_WRITES
 			| (DEFAULT_PANIC_PRIORITY << CS_PANIC_PRIORITY_SHIFT)
@@ -721,11 +722,11 @@ void BCM_PCM::start()
 		write32(ARM_PCM_CS_A, read32(ARM_PCM_CS_A) | CS_A_TXON);
 		CTimer::Get()->usDelay(10);
 	}
-	
+
 	// we are running ..
-	
+
 	m_state = bcmSoundRunning;
-	
+
 }
 
 
@@ -741,7 +742,7 @@ void BCM_PCM::updateInput(bool cold)
 	// called twice during setup with cold=1, leaves
 	// the toggle at zero.  On the first interrupt
 	// buffer[0] will be ready while buffer[1] is being
-	// filled. 
+	// filled.
 {
 	// pass the finished ready buffer to client
 	// and reset it into the DMA as the next buffer
@@ -770,7 +771,7 @@ void BCM_PCM::updateOutput(bool cold)
 {
 	// get the next client output buffer from the client
 	// and give the output buffer to the DMA to output
-	
+
 	if (!cold)
 	{
 		#if OUTPUT_DISTINCTIVE_PATTERN
@@ -810,7 +811,7 @@ void BCM_PCM::updateOutput(bool cold)
 	}
 	CleanAndInvalidateDataCacheRange((uintptr) m_outBuffer[m_outToggle], RAW_AUDIO_BLOCK_BYTES);
 	m_outToggle ^= 1;
-}	
+}
 
 
 
@@ -840,7 +841,7 @@ void BCM_PCM::audioInIRQ(void)
 	u32 nIntMask = 1 << m_nDMAInChannel;
 	u32 nOtherMask = 1 << m_nDMAOutChannel;
 	u32 nIntStatus = read32(ARM_DMA_INT_STATUS);
-	
+
 	if (!(nIntStatus & nIntMask))
 	{
 		if (nIntStatus & nOtherMask)
@@ -856,21 +857,20 @@ void BCM_PCM::audioInIRQ(void)
 			in_wrong_count++;
 			PeripheralExit();
 		}
-		return;		
+		return;
 	}
-	
+
 	in_block_count++;
 	write32(ARM_DMA_INT_STATUS, nIntMask);
 	u32 nCS = read32(ARM_DMACHAN_CS(m_nDMAInChannel));
 	assert(nCS & CS_INT);
 	write32(ARM_DMACHAN_CS(m_nDMAInChannel), nCS);	// reset CS_INT
 	PeripheralExit();
-	
-	#if INCLUDE_ACTIVITY_LEDS		
+
+	#if INCLUDE_ACTIVITY_LEDS
 		static u32 rx_count = 0;
 		rx_count++;
-		if (rx_count > 32)
-			// about 10 times a second at 44.1khz with 128 sized buffers
+		if (rx_count > CALLS_PER_LED_FLASH)
 		{
 			rx_count = 0;
 			m_RX_ACTIVE.Invert();
@@ -891,7 +891,7 @@ void BCM_PCM::audioInIRQ(void)
 		case bcmSoundRunning:
 			updateInput(false);
 			break;
-	
+
 		case bcmSoundCancelled:
 			PeripheralEntry();
 			write32(ARM_DMACHAN_NEXTCONBK(m_nDMAInChannel), 0);
@@ -899,11 +899,11 @@ void BCM_PCM::audioInIRQ(void)
 			PeripheralExit();
 			m_state = bcmSoundTerminating;
 			break;
-	
+
 		case bcmSoundTerminating:
 			m_state = bcmSoundIdle;
 			break;
-	
+
 		default:
 			assert(0);
 			break;
@@ -920,7 +920,7 @@ void BCM_PCM::audioOutIRQ(void)
 	u32 nIntMask = 1 << m_nDMAOutChannel;
 	u32 nOtherMask = 1 << m_nDMAInChannel;
 	u32 nIntStatus = read32(ARM_DMA_INT_STATUS);
-	
+
 	if (!(nIntStatus & nIntMask))
 	{
 		if (nIntStatus & nOtherMask)
@@ -934,26 +934,26 @@ void BCM_PCM::audioOutIRQ(void)
 			out_wrong_count++;
 			PeripheralExit();
 		}
-		return;		
+		return;
 	}
-	
+
 	out_block_count++;
 	write32(ARM_DMA_INT_STATUS, nIntMask);
 	u32 nCS = read32(ARM_DMACHAN_CS(m_nDMAOutChannel));
 	assert(nCS & CS_INT);
 	write32(ARM_DMACHAN_CS(m_nDMAOutChannel), nCS);	// reset CS_INT
 	PeripheralExit();
-	
-	#if INCLUDE_ACTIVITY_LEDS		
+
+	#if INCLUDE_ACTIVITY_LEDS
 		static u32 tx_count = 0;
 		tx_count++;
-		if (tx_count > 32)		// about 20 times a second
+		if (tx_count > CALLS_PER_LED_FLASH)
 		{
 			tx_count = 0;
 			m_TX_ACTIVE.Invert();
 		}
 	#endif
-	
+
 	if (nCS & CS_ERROR)
 	{
 		LOG_ERROR("CS_ERROR in audioOutIRQ()",0);
@@ -968,18 +968,18 @@ void BCM_PCM::audioOutIRQ(void)
 		case bcmSoundRunning:
 			updateOutput(false);
 			break;
-	
+
 		case bcmSoundCancelled:
 			PeripheralEntry();
 			write32(ARM_DMACHAN_NEXTCONBK(m_nDMAOutChannel), 0);
 			PeripheralExit();
 			m_state = bcmSoundTerminating;
 			break;
-	
+
 		case bcmSoundTerminating:
 			m_state = bcmSoundIdle;
 			break;
-	
+
 		default:
 			assert(0);
 			break;
@@ -987,5 +987,3 @@ void BCM_PCM::audioOutIRQ(void)
 
 	m_SpinLock.Release();
 }
-
-
